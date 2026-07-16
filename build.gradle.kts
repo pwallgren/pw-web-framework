07f374e845b7932d043e3a6596c84ccb3383b67a
Petter Wallgren <petter9393@hotmail.com>
2026-07-16 13:25:12 +0200

    Add local maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    `maven-publish`
 }
 
 group = "com.petwal"
@@ -8,6 +9,10 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("http://192.168.0.22:8080/releases")
+        isAllowInsecureProtocol = true
+    }
 }
 
 java {
@@ -25,4 +30,29 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.38")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.22.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "reposilite"
+            url = uri(
+                if (version.toString().endsWith("-SNAPSHOT")) {
+                    "http://pw-pi-1:8080/snapshots"
+                } else {
+                    "http://pw-pi-1:8080/releases"
+                }
+            )
+            credentials {
+                username = findProperty("reposiliteUser") as String? ?: System.getenv("REPOSILITE_USER")
+                password = findProperty("reposilitePassword") as String? ?: System.getenv("REPOSILITE_PASSWORD")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
 }
